a2719777b6817be7632f27bc06303870962641f0
Butya <Butya@192.168.127.1>
2016-01-28 09:00:53 +0200

    add !!!

--- Git_testProgect/src/by/savin/test/Main.java
+++ Git_testProgect/src/by/savin/test/Main.java
@@ -2,6 +2,6 @@ package by.savin.test;
 
 public class Main {
 	public static void main(String[] args){
-		System.out.println("Hello from github");
+		System.out.println("Hello from github!!!");
 	}
 }
