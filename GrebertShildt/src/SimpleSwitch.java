21a5ad97d5451699c232c9edc765e0e37d17cf9d
Butya <Butya@192.168.127.1>
2016-01-28 12:07:50 +0200

    1

---
+++ GrebertShildt/src/SimpleSwitch.java
@@ -0,0 +1,28 @@
+
+public class SimpleSwitch {
+
+	public static void main(String[] args) {
+		for(int i=0;i<6;i++)
+			switch(i){
+			case 0:
+				System.out.println("i equals "+i);
+				break;
+			case 1:
+				System.out.println("i equals "+i);
+				break;
+			case 2:
+				System.out.println("i equals "+i);
+				break;
+			case 3:
+				System.out.println("i equals "+i);
+				break;
+			case 4:
+				System.out.println("i equals "+i);
+				break;
+			case 5:
+				System.out.println("i equals "+i);
+				break;
+		}
+	}
+
+}
